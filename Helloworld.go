0c81db257d666f1c08a0684ed02cc3596c2afa0c
FerdyJumah <150941402+FerdyJumah@users.noreply.github.com>
2024-02-02 22:01:32 +0300

    Create Helloworld.go

---
+++ Helloworld.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Helloworld")
+	fmt.Println("Ferdinand Juma")
+	fmt.Println("I will buy a BMW X6")
+	fmt.Println("I owe myself a vacation")
+}
